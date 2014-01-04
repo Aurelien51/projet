480ef88eee195931b15d6fd0e24d09b87ddda313
Euregan <euregan@gmail.com>
2014-01-04 15:21:43 +0100

    Création des fenêtres de jeu
    Création des fenêtres de jeu
    Mise en place du menu d'accueil

---
+++ ChiffresEtLettres/ChiffresEtLettres/CompteEstBon.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChiffresEtLettres
+{
+    /// <summary>
+    /// Interaction logic for CompteEstBon.xaml
+    /// </summary>
+    public partial class CompteEstBon : Window
+    {
+        public CompteEstBon()
+        {
+            InitializeComponent();
+        }
+    }
+}
