2674050b8774c062f3dda0047aa1ad06cee048d7
Jérémy GIGNON <jeremy.gignon@gmail.com>
2014-01-08 00:14:54 +0100

    Added all words random searches

--- Countdown/Countdown/LongestWord.cs
+++ Countdown/Countdown/LongestWord.cs
@@ -43,7 +43,23 @@ namespace Countdown
             this.lettersAvailable = this.lettersAvailable.OrderBy(x => random.Next()).ToArray();
             MessageBox.Show("Lettres: " + new String(this.lettersAvailable));
 
-            String query = "SELECT word FROM dictionary WHERE word LIKE 'aboie%' ORDER BY LENGTH(word) DESC LIMIT 5";
+            String query = "SELECT word FROM dictionary";
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 0)
+                {
+                    query += " WHERE";
+                }
+                else
+                {
+                    query += " AND";
+                }
+
+                query += " word LIKE '%" + this.lettersAvailable[i] + "%'";
+
+            }
+            query += " ORDER BY LENGTH(word) DESC LIMIT 5";
+            //MessageBox.Show(query);
             DataTable list = this.database.GetDataTable(query);
 
             foreach (DataRow row in list.Rows)
